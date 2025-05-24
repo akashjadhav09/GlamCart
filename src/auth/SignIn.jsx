5044de905abcd49dcca8097419a68c6d9248f02d
Akash Jadhav <152849719+akashjadhav09@users.noreply.github.com>
2025-05-24 23:29:51 +0530

    Login functionality.

--- src/auth/SignIn.jsx
+++ src/auth/SignIn.jsx
@@ -1,22 +1,50 @@
-import {useState} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import './css/signIn.css'
 import { MdOutlineVisibility, MdOutlineVisibilityOff } from "react-icons/md";
+import { ProductContext } from '../context/ProductData';
 
 export default function SignInForm () {
     const [showPassword, setShowPassword] = useState(false);
-
+    const [userName, setUserName] = useState('');
+    const [userPass, setUserPass] = useState('');
+    const [storedUserData, setStoredUserData] = useState([]);
+    const { validUser, setValidUser } = useContext(ProductContext);
+    
     const navigate = useNavigate();
 
+
+    useEffect(()=>{
+        setStoredUserData(JSON.parse(localStorage.getItem("users")) || []);
+        if (validUser.length > 0) {
+            navigate('/home', { replace: true }); 
+        }   
+    },[validUser])
+    
     const handleLogin = () => {
-      // Perform login logic
-      navigate('/'); // Redirect to home after login
+        const matchedUser = storedUserData.find((user) =>
+            user.userid === userName && user.pass === userPass
+        );
+        if(matchedUser){
+            alert("logged in...");
+            setValidUser([matchedUser]); 
+            navigate('/home');
+            navigate('/', { replace: true });
+        }else{
+            alert("Invalid credentials...");
+        }
+
+        clearForm();
     };
 
     function handleRouteToSignUp(){
-        console.log('Handle route')
         navigate('/signup');
+    }
+
+    function clearForm(){
+        setUserName('');
+        setUserPass('');
     }
 
     return(
@@ -36,25 +64,35 @@ export default function SignInForm () {
                    </div>
                     <div className='credential-wrapper'>
                         <h4>Username</h4>
-                        <input  type="text" className="user-input input-type__username" required/>
+                        <input  type="text"
+                            className="user-input input-type__username" 
+                            value={userName}
+                            onChange={(e)=>setUserName(e.target.value)}
+                            required/>
 
                         <h4>Password</h4>
                         <div className='password-wrapper'>
-                            <input  type={ showPassword ? "password" : "text" }  minLength="4" maxLength="10" className="user-input input-type__password" required/>
+                            <input  type={ showPassword ? "text" : "password" } 
+                                minLength="4" maxLength="10" 
+                                className="user-input input-type__password" 
+                                value={userPass}
+                                onChange={(e)=>setUserPass(e.target.value)}
+                                required/>
+
                             {showPassword ? 
                                 (<span className='show-password-icon' onClick={() => setShowPassword(false)}>
-                                    <MdOutlineVisibility />
+                                    <MdOutlineVisibilityOff/>
                                 </span>) 
                                 : 
                                 (<span className='show-password-icon' onClick={() => setShowPassword(true)}>
-                                    <MdOutlineVisibilityOff/>
+                                    <MdOutlineVisibility />
                                 </span>)
                             }
                             
                         </div>
                     </div>
                     <div className='forgot-password-msg'>Forgot Password?</div>
-                    <button className='login-button'>Login</button>
+                    <button className='login-button' onClick={handleLogin}>Login</button>
                     <div className='signin-required-wrapper'>
                         <div className='signin-required-msg'>Dont't have an account?<span className='signup-label' onClick={handleRouteToSignUp}> Signup</span></div>
                     </div>
